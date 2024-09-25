77534e1cc2a5d494782eea55e8764b77010b7680
AndersonKerstin <50719203+AndersonKerstin@users.noreply.github.com>
2024-09-25 17:16:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,12 @@ async function connectMetaMask() {
         alert('MetaMask não está instalada!');
     }
 }
+
+/// Verificar posse do NFT
+async function checkNFTOwnership(account, contractAddress, tokenId) {
+    const web3 = new web3(window.ethereum);
+    const contract = new web3.eth.Contract(abi, contractAddress);
+    const balance = await contract.methods.balanceOf(account, tokenId).call();
+    return balance > 0;
+
+}
